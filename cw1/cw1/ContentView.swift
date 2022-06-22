67e6bf8eb70dcf9e0f1f6b1f5e42e0af14798092
leenaaljassar-529 <66418005+leenaaljassar-529@users.noreply.github.com>
2022-06-22 19:34:39 +0300

    cw 1

---
+++ cw1/cw1/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  cw1
+//
+//  Created by Leena Al Jassar on 22/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+          Color(hue: 0.535, saturation: 0.236, brightness: 0.884)
+                .ignoresSafeArea()
+            VStack{
+                Text("Hello name is Leena")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .padding()
+                Text("I'm 18 years old")
+                    .font(.title2)
+                    .padding()
+                Text("I'm learning SWiftUI now!")
+                    .padding()
+                HStack{
+                    Spacer()
+                    Text("🌻")
+                        .font(.largeTitle)
+                    Spacer()
+                    Text("⭐️")
+                        .font(.largeTitle)
+                    Spacer()
+                    Text("🌿")
+                        .font(.largeTitle)
+                    Spacer()
+                }
+          
+        }
+            
+            
+        }
+        
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw1/cw1/cw1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw1App.swift
+//  cw1
+//
+//  Created by Leena Al Jassar on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
